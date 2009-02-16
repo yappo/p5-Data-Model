3d229b797001e25db916c90dfaedd8f6c37dd685
大沢 和宏 <yappo>
2009-02-16 21:05:15 +0900

    not used registered column name of mysql

--- t/lib/Mock/Tests/Default.pm
+++ t/lib/Mock/Tests/Default.pm
@@ -8,35 +8,35 @@ sub t_01_not_set : Tests(10) {
     my $set = mock->set( 'tbl' );
     isa_ok $set, mock_class."::tbl";
     is $set->id, 1, 'id';
-    is $set->int1, 10, 'int';
-    is $set->char1, 'foo', 'char';
+    is $set->c_int1, 10, 'int';
+    is $set->c_char1, 'foo', 'char';
     is $set->code1, 'bar', 'code';
 
     my($get) = mock->get( 'tbl' => 1 );
     isa_ok $get, mock_class."::tbl";
     is $get->id, 1, 'id';
-    is $get->int1, 10, 'int';
-    is $get->char1, 'foo', 'char';
+    is $get->c_int1, 10, 'int';
+    is $get->c_char1, 'foo', 'char';
     is $get->code1, 'bar', 'code';
 }
 
 sub t_01_set_values : Tests(10) {
     my $set = mock->set( 'tbl' => {
-        int1  => 99,
-        char1 => 'kyuukyuu',
+        c_int1  => 99,
+        c_char1 => 'kyuukyuu',
         code1 => 'code',
     });
     isa_ok $set, mock_class."::tbl";
     is $set->id, 2, 'id';
-    is $set->int1, 99, 'int';
-    is $set->char1, 'kyuukyuu', 'char';
+    is $set->c_int1, 99, 'int';
+    is $set->c_char1, 'kyuukyuu', 'char';
     is $set->code1, 'code', 'code';
 
     my($get) = mock->get( 'tbl' => 2 );
     isa_ok $get, mock_class."::tbl";
     is $get->id, 2, 'id';
-    is $get->int1, 99, 'int';
-    is $get->char1, 'kyuukyuu', 'char';
+    is $get->c_int1, 99, 'int';
+    is $get->c_char1, 'kyuukyuu', 'char';
     is $get->code1, 'code', 'code';
 }
 
