963b94d4c4fce805371f1ff73ce9cbedabe0e922
yappo <yappo@d0d07461-0603-4401-acd4-de1884942a52>
2009-02-19 10:32:15 +0000

    check all bytes test for binary
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Data-Model/trunk@30281 d0d07461-0603-4401-acd4-de1884942a52

--- t/lib/Mock/Tests/Binary.pm
+++ t/lib/Mock/Tests/Binary.pm
@@ -4,25 +4,33 @@ use base 'Test::Class';
 use Mock::Tests;
 use Test::More;
 
-my $bin1 = pack 'C*', qw( 10 2 43 0 9 84 45 255 );
-sub t_01_set : Tests(4) {
-    my $set = mock->set( model => 1 => { data => $bin1 } );
-    isa_ok $set, mock_class."::model";
-    is $set->id, 1, 'id';
-    is $set->data, $bin1, 'binary data';
+my @list = qw( 10 2 43 0 9 84 45 255 );
+sub t_01_set : Tests(1024) {
+    for my $i (0..255) {
+        local $list[3] = $i;
+        my $bin = pack 'C*', @list; 
+       my $set = mock->set( model => $i+1 => { data => $bin } );
+        isa_ok $set, mock_class."::model";
+        is $set->id, $i+1, 'id';
+        is $set->data, $bin, 'binary data';
 
-    use bytes;
-    is bytes::length($set->data), 8, 'length';
+        use bytes;
+        is bytes::length($set->data), 8, 'length';
+    }
 }
 
-sub t_02_get : Tests(4) {
-    my($get) = mock->get( model => 1 );
-    isa_ok $get, mock_class."::model";
-    is $get->id, 1, 'id';
-    is $get->data, $bin1, 'binary data';
+sub t_02_get : Tests(1024) {
+    for my $i (0..255) {
+        local $list[3] = $i;
+        my $bin = pack 'C*', @list;
+        my($get) = mock->get( model => $i+1 );
+        isa_ok $get, mock_class."::model";
+        is $get->id, $i+1, 'id';
+        is $get->data, $bin, 'binary data';
 
-    use bytes;
-    is bytes::length($get->data), 8, 'length';
+        use bytes;
+        is bytes::length($get->data), 8, 'length';
+    }
 }
 
 1;
