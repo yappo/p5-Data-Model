47f87207db8b0be7babe9e48874a03dddc0b8006
yappo <yappo@shibuya.pl>
2009-06-24 17:51:45 +0900

    add customizable serializer for Driver::Memcached

--- lib/Data/Model/Driver/Memcached.pm
+++ lib/Data/Model/Driver/Memcached.pm
@@ -11,11 +11,27 @@ sub memcached { shift->{memcached} }
 
 sub update_direct { Carp::croak("update_direct is NOT IMPLEMENTED") }
 
+sub init {
+    my $self = shift;
+    if (my $serializer = $self->{serializer}) {
+        $serializer = 'Data::Model::Driver::Memcached::Serializer::' . $serializer
+            unless $serializer =~ s/^\+//;
+        unless ($serializer eq 'Data::Model::Driver::Memcached::Serializer::Default') {
+            eval "use $serializer"; ## no critic
+            Carp::croak $@;
+        }
+        $self->{serializer} = $serializer;
+    }
+}
+
 sub lookup {
     my($self, $schema, $key) = @_;
     my $cache_key = $self->cache_key($schema, $key);
     my $ret = $self->{memcached}->get( $cache_key );
     return unless $ret;
+    if ($self->{serializer}) {
+        $ret = $self->{serializer}->deserialize($self, $ret);
+    }
     return $ret;
 }
 
@@ -27,6 +43,9 @@ sub lookup_multi {
 
     my %resultlist;
     while (my($id, $data) = each %{ $ret }) {
+        if ($self->{serializer}) {
+            $data = $self->{serializer}->deserialize($self, $data);
+        }
         my $key = $schema->get_key_array_by_hash($data);
         $resultlist{join "\0", @{ $key }} = +{ %{ $data } };
     }
@@ -39,7 +58,9 @@ sub get {
     my $cache_key = $self->cache_key($schema, $key);
     my $ret = $self->{memcached}->get( $cache_key );
     return unless $ret;
-
+    if ($self->{serializer}) {
+        $ret = $self->{serializer}->deserialize($self, $ret);
+    }
     return $self->_generate_result_iterator([ $ret ]), +{};
 }
 
@@ -47,7 +68,11 @@ sub set {
     my($self, $schema, $key, $columns, %args) = @_;
 
     my $cache_key = $self->cache_key($schema, $key);
-    my $ret = $self->{memcached}->add( $cache_key, $columns );
+    my $data = $columns;
+    if ($self->{serializer}) {
+        $data = $self->{serializer}->serialize($self, $columns);
+    }
+    my $ret = $self->{memcached}->add( $cache_key, $data );
     return unless $ret;
 
     $columns;
@@ -57,7 +82,11 @@ sub replace {
     my($self, $schema, $key, $columns, %args) = @_;
 
     my $cache_key = $self->cache_key($schema, $key);
-    my $ret = $self->{memcached}->set( $cache_key, $columns );
+    my $data = $columns;
+    if ($self->{serializer}) {
+        $data = $self->{serializer}->serialize($self, $columns);
+    }
+    my $ret = $self->{memcached}->set( $cache_key, $data );
     return unless $ret;
 
     $columns;
@@ -73,7 +102,11 @@ sub update {
         return unless $ret;
     }
 
-    my $ret = $self->{memcached}->set( $new_cache_key, $columns );
+    my $data = $columns;
+    if ($self->{serializer}) {
+        $data = $self->{serializer}->serialize($self, $columns);
+    }
+    my $ret = $self->{memcached}->set( $new_cache_key, $data );
     return unless $ret;
 
     $columns;
@@ -87,6 +120,143 @@ sub delete {
     my $ret = $self->{memcached}->delete( $cache_key );
     return unless $ret;
     $data;
+}
+
+package
+    Data::Model::Driver::Memcached::Serializer::Default;
+# serializer use messagepack format
+# implement format is map16, map32, fixmap and nil, raw16, rwa32, fixraw
+# see http://msgpack.sourceforge.jp/spec
+use strict;
+use warnings;
+use Carp ();
+$Carp::Internal{(__PACKAGE__)}++;
+
+my $MAGIC = 'd'^'e'^'f'^'a'^'u'^'l'^'t';
+my $MAP16 = pack 'C', 0xde;
+my $MAP32 = pack 'C', 0xdf;
+my $RAW16 = pack 'C', 0xda;
+my $RAW32 = pack 'C', 0xdb;
+my $NIL   = pack 'C', 0xc0;
+
+sub serialize {
+    my($class, $c, $hash) = @_;
+    Carp::croak "usage: $class->serialize(\$self, \$hashref)" unless ref($hash) eq 'HASH';
+    my $num = scalar(keys(%{ $hash }));
+    Carp::croak "this serializer work is under 2^32 columns" if $num >= (2**32);
+
+    my $pack = $MAGIC;
+    if ($num < 16) {
+        # FixMap
+        $pack .= pack 'C', (0x80 + $num);
+    } elsif ($num < 256*256) {
+        # map16
+        $pack .= $MAP16 . pack('n', $num);
+   } else {
+        # map32
+        $pack .= $MAP32 . pack('N', $num);
+    }
+
+    while (my($k, $v) = each %{ $hash }) {
+        if (defined $k) {
+            my $l = length($k);
+            if ($l < 32) {
+                $pack .= pack 'C', 0xa0 + $l;
+            } elsif ($l < 256*256) {
+                $pack .= $RAW16 . pack('n', $l);
+            } elsif ($l < 2**32) {
+                $pack .= $RAW32 . pack('N', $l);
+            } else {
+                Carp::croak "this serializer work is under 2^32 length ($k => $v)";
+            }
+            $pack .= $k;
+        } else {
+            # undef
+            $pack .= $NIL;
+        }
+
+        if (defined $v) {
+            my $l = length($v);
+            if ($l < 32) {
+                $pack .= pack 'C', 0xa0 + $l;
+            } elsif ($l < 256*256) {
+                $pack .= $RAW16 . pack('n', $l);
+            } elsif ($l < 2**32) {
+                $pack .= $RAW32 . pack('N', $l);
+            } else {
+                Carp::croak "this serializer work is under 2^32 length ($k => $v)";
+            }
+            $pack .= $v;
+        } else {
+            # undef
+            $pack .= $NIL;
+        }
+    }
+
+    $pack;
+}
+
+sub deserialize {
+    my($class, $c, $pack) = @_;
+    $pack =~ s/^(.)//;
+    my $fmt = $1;
+    Carp::croak "this pack data is not Default format" unless $fmt eq $MAGIC;
+
+    my $pos = 0;
+    my $len = length($pack);
+
+    # unpack hash header
+    my $map_type = substr($pack, $pos++, 1);
+    my $elements = 0;
+    if ($map_type eq $MAP16) {
+        $elements = unpack 'n', substr($pack, $pos);
+        $pos += 2;
+    } elsif ($map_type eq $MAP32) {
+        $elements = unpack 'N', substr($pack, $pos);
+        $pos += 4;
+    } else {
+        # under 16 elements
+        $elements = unpack 'C', $map_type;
+        $elements -= 0x80;
+        Carp::croak "extra bytes" if $elements >= 16;
+    }
+
+    # unpack for map elements
+    my $hash = +{};
+    for (1..$elements) {
+        my $k;
+        for (0..1) {
+            my $v;
+            my $len;
+
+            my $data_type = substr($pack, $pos++, 1);
+            if ($data_type eq $NIL) {
+                $v = undef;
+            } else {
+                if ($data_type eq $RAW16) {
+                    $len = unpack 'n', substr($pack, $pos);
+                    $pos += 2;
+                } elsif ($data_type eq $RAW32) {
+                    $len = unpack 'N', substr($pack, $pos);
+                    $pos += 4;
+                } else {
+                    $len = unpack 'C', $data_type;
+                    $len -= 0xa0;
+                    Carp::croak "extra bytes" if $len >= 32;
+                }
+                $v = substr($pack, $pos, $len);
+                $pos += $len;
+            }
+
+            if ($_) {
+                $hash->{$k} = $v;
+            } else {
+                $k = $v;
+            }
+        }
+    }
+
+    $hash;
 }
 
 1;
@@ -111,6 +281,33 @@ Data::Model::Driver::Memcached - storage driver for memcached protocol
   install_model model_name => schema {
     ....
   };
+
+
+use customizable serializer
+
+  my $driver = Data::Model::Driver::Memcached->new(
+      memcached  => Cache::Memcached::Fast->new({ servers => [ { address => "localhost:11211" }, ], }),
+      serializer => 'Default', # default is messagepack minimum set for Data::Model
+  );
+
+  {
+      package MySerializer;
+      sub serialize {
+          my($class, $c, $hash) = @_;
+          # you serialize of $hash
+          return $serialize_string;
+      }
+      sub deserialize {
+          my($class, $c, $serialize_string) = @_;
+          ...
+          return $hash;
+      }
+  }
+  my $driver = Data::Model::Driver::Memcached->new(
+      memcached  => Cache::Memcached::Fast->new({ servers => [ { address => "localhost:11211" }, ], }),
+      serializer => '+MySerializer',
+  );
+
 
 =head1 DESCRIPTION
 
